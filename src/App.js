a49ef495ea8e78119b912017761975e2b52e8b16
Edwin-niwd3 <edwin.navarro@laverne.edu>
2024-04-13 14:40:26 -0700

    Remove App.CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
 
   const title = 'Welcome to the new blog thingy';
+  const likes = 50;
 
   return (
     <div className="App">
+      <Navbar>
+      </Navbar>
+      <Home></Home>
      <div className="content">
       <h1>{ title }</h1>
+      <p>Liked { likes } times</p>
+
+      <p> { 10 } </p>
+      <p> { Math.random() * 10 } </p>
      </div>
     </div>
   );
