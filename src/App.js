8a13815302cc01f8950c0341436eb7dfa9144f37
Edwin-niwd3 <edwin.navarro@laverne.edu>
2024-04-13 21:21:05 -0700

    Shorten code, and make button

--- src/App.js
+++ src/App.js
@@ -3,20 +3,13 @@ import Home from "./Home";
 
 function App() {
 
-  const title = 'Welcome to the new blog thingy';
-  const likes = 50;
 
   return (
     <div className="App">
       <Navbar>
       </Navbar>
-      <Home></Home>
      <div className="content">
-      <h1>{ title }</h1>
-      <p>Liked { likes } times</p>
-
-      <p> { 10 } </p>
-      <p> { Math.random() * 10 } </p>
+     <Home></Home>
      </div>
     </div>
   );
