606490cd9cc182118b3bbf9c41e0236b885164f8
Edwin-niwd3 <edwin.navarro@laverne.edu>
2024-04-19 22:22:52 -0700

    handleDelete no longer works permanently

--- src/Home.js
+++ src/Home.js
@@ -3,34 +3,28 @@ import BlogList from './blogList';
 
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-      { title: 'My new website', author: 'mario', id: 1 },
-      { title: 'Welcome party!', author: 'yoshi', id: 20 },
-      { title: 'Web dev top tips', author: 'mario', id: 3 }
-  ])
+  const [blogs, setBlogs] = useState(null)
 
   const [name, setName] = useState('mario');
 
-  const handleDelete = (id) => {
-    //does not mutate original data yet
-    //only return true if the id does not match the one we want to remove
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs);
-  }
-
   //runs everytime there is a re-render (if any data changes)
   useEffect(() => {
-    console.log('name has been changed')
-  }, [name]);
+    fetch('http://localhost:8000/blogs')
+    .then(res => {
+      return res.json();
+    })
+    .then ((data)=> {
+      console.log(data);
+      setBlogs(data);
+    })
+  }, []);
   //The empty array means that we only do it on the first initial render
   //We can also add dependancies.
   //This means that it will only run the effect on specific conditions we set
   
   return ( 
     <div className="home">
-     <BlogList blogs = {blogs} handleDelete = {handleDelete} />
-     <button onClick = {() => setName('Luigi')}>change name</button>
-     <p>{name}</p>
+     {blogs && <BlogList blogs = {blogs} />}
     </div>
    );
 }
